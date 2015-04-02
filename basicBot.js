a40844f7491f0e8b5691c80926d4f8285dbb8c9c
reidon <michael.rene@live.dk>
2015-04-02 03:53:06 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -178,7 +178,7 @@
     var botMaintainer = "Benzi (Quoona)"
     var botCreatorIDs = ["3851534", "4105209"];
 
-    var basicBot = {
+    var Reidon's Über awesome bot = {
         version: "42",
         status: false,
         name: "Reidon's Über awesome bot",
