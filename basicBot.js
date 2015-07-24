de9be9c217e669f1210a640f9183d8221397236c
reidon <michael.rene@live.dk>
2015-07-24 20:21:46 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1964,7 +1964,7 @@
                     'gives you a Senor cookie. It reads “Don’t take him seriously”',
                     'gives you a burnt cookie.  Sorry I don’t know how to bake.'
                 ],
-                
+           }
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
