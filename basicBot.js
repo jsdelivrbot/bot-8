c14ff85e26beb21eab004bfd014ea81c5dc0767c
reidon <michael.rene@live.dk>
2015-07-24 20:35:39 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "69",
+        version: "pik",
         status: false,
         name: "basicBot",
         loggedInID: null,
